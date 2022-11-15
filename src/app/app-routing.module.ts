c23e234a4f724812fc9a76d6f26572c8cfa5c3ee
AleC <aleobera20@gmail.com>
2022-11-15 00:45:54 -0300

    Deploy en ng-node

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './componentes/home/home.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/service/consulta.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Consultas } from "../models/consultas";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConsultaService {
+
+  // URL: string = 'http://localhost:3000/'
+  
+  URL: string = 'https://angular-app-2022.herokuapp.com/api/'
+
+  // URL = environment.URL;
+  
+  constructor(private http: HttpClient) { }
+
+
+  guardarConsulta(consulta: Consultas): Observable<any> {
+    return this.http.post(this.URL, consulta)
+  }
+
+}
